6e3f710e0a96ff0a4efd2eb525e664f876532ab1
cesar galvis <cgalvist@gmail.com>
2016-08-27 17:07:14 -0500

    primera versión de triqui funcionando

---
+++ retos/reto 3/TriquiCesar/app/src/main/java/cgt/triquicesar/MainActivityTriqui.java
@@ -0,0 +1,118 @@
+package cgt.triquicesar;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivityTriqui extends AppCompatActivity {
+
+    private TriquiJuego mJuego;
+
+    // Buttons making up the board
+    private Button mBoardButtons[];
+
+    // Various text displayed
+    private TextView mInfoTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_triqui);
+
+        mBoardButtons = new Button[TriquiJuego.BOARD_SIZE];
+        mBoardButtons[0] = (Button) findViewById(R.id.one);
+        mBoardButtons[1] = (Button) findViewById(R.id.two);
+        mBoardButtons[2] = (Button) findViewById(R.id.three);
+        mBoardButtons[3] = (Button) findViewById(R.id.four);
+        mBoardButtons[4] = (Button) findViewById(R.id.five);
+        mBoardButtons[5] = (Button) findViewById(R.id.six);
+        mBoardButtons[6] = (Button) findViewById(R.id.seven);
+        mBoardButtons[7] = (Button) findViewById(R.id.eight);
+        mBoardButtons[8] = (Button) findViewById(R.id.nine);
+
+        mInfoTextView = (TextView) findViewById(R.id.information);
+
+        mJuego = new TriquiJuego();
+
+        startNewGame();
+    }
+
+    // Set up the game board.
+    private void startNewGame() {
+
+        mJuego.clearBoard();
+
+        // Reset all buttons
+        for (int i = 0; i < mBoardButtons.length; i++) {
+            mBoardButtons[i].setText("");
+            mBoardButtons[i].setEnabled(true);
+            mBoardButtons[i].setOnClickListener(new ButtonClickListener(i));
+        }
+
+        // Human goes first
+        mInfoTextView.setText("You go first.");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add("New Game");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        startNewGame();
+        return true;
+    }
+
+    private void setMove(char player, int location) {
+
+        mJuego.setMove(player, location);
+        mBoardButtons[location].setEnabled(false);
+        mBoardButtons[location].setText(String.valueOf(player));
+        if (player == TriquiJuego.HUMAN_PLAYER)
+            mBoardButtons[location].setTextColor(Color.rgb(0, 200, 0));
+        else
+            mBoardButtons[location].setTextColor(Color.rgb(200, 0, 0));
+    }
+
+    // Handles clicks on the game board buttons
+    public class ButtonClickListener implements View.OnClickListener {
+        int location;
+
+        public ButtonClickListener(int location) {
+            this.location = location;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mBoardButtons[location].isEnabled()) {
+                setMove(TriquiJuego.HUMAN_PLAYER, location);
+
+                // If no winner yet, let the computer make a move
+                int winner = mJuego.checkForWinner();
+                if (winner == 0) {
+                    mInfoTextView.setText(R.string.turn_computer);
+                    int move = mJuego.getComputerMove();
+                    setMove(TriquiJuego.COMPUTER_PLAYER, move);
+                    winner = mJuego.checkForWinner();
+                }
+
+                if (winner == 0)
+                    mInfoTextView.setText(R.string.turn_human);
+                else if (winner == 1)
+                    mInfoTextView.setText(R.string.result_tie);
+                else if (winner == 2)
+                    mInfoTextView.setText(R.string.result_human_wins);
+                else
+                    mInfoTextView.setText(R.string.result_computer_wins);
+            }
+        }
+    }
+}
